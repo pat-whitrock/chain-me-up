77f7cd485fa1fe50667e83a17ff5e2176d5a705c
georgemayer <mayer.georgep@gmail.com>
2014-03-13 14:49:37 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChainMeUp::Application.config.secret_key_base = '2872b358f7b905c5374373c7f5290cdc76dd775ec4e49f446b41126c0a90b324938192f171b160458d0311f943825bafb585553f5ba2e389e3e8ed42aad2274f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChainMeUp::Application.config.session_store :cookie_store, key: '_chain_me_up_session'
