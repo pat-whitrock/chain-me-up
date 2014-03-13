a57b46f74b8ba9a1d841a07844817d4987e9e799
georgemayer <mayer.georgep@gmail.com>
2014-03-13 15:20:18 -0400

    tree model

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,4 +1,13 @@
 class Tree
   include Mongoid::Document
 
+  field :first_name, type: String
+  field :last_name, type: String
+  field :children, type: Array
+
+  def initialize 
+    @children = []
+  end
+
+
 end
