e1d29484830d42e104069941a462954a1e0a689e
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-03-19 15:03:37 -0400

    moving over all code to a new mongoid-tree branch

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -8,6 +8,27 @@ class Tree
   field :content, type: String
 
   def construct_history
+    self.ancestors_and_self.collect do |node|
+      node.content
+    end.join(" ")
+  end
+
+  def set_children
+    @children = {}
+  end
+
+  # def view_children(hash)
+  #   self.children.each do |child|
+  #     hash[self] << child      
+  #     hash = hash[self]
+  #     view_children(hash[child])
+  #   end
+  # end
+
+# { 0 : [Tree1,Tree2,Tree3], 
+  
+#}
+
   end
 
   def has_children?
