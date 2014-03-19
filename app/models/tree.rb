3014560de866e12698bca82ed1cb5f6af3b24ade
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-03-19 14:16:42 -0400

    cleaned up tree.rb to work solely with Mongoid::Tree. Added benchmark tests to seeds.rb

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -4,71 +4,14 @@ class Tree
   include Mongoid::Document
   include Mongoid::Tree
 
-#  recursively_embeds_many
-  after_initialize :initialize_history
-
   field :user_id, type: Integer
   field :content, type: String
 
-  attr_accessor :history
-
-
-  def initialize_history
-    @history = []
-  end
-
-  def find_node_by_id
-  end
-
-
-  def get_history
-    history = []
-    self.traverse_parents do |node|
-      history << node
-    end
-    history 
-  end
-
-  def traverse_parents(&block)
-    yield self
-    if !self.parent.nil?
-      self.parent.traverse_parents(&block)
-    end  
-  end
-
   def construct_history
-    history_array = get_history.reverse
-    story = ""
-    history_array.each do |history|
-      story << history.content
-    end
-    story
-  end
-
-  def self.find_by_id(tree_id)
-    id = BSON::ObjectId.from_string(tree_id)
-    tree = Tree.first
-    goal = nil
-    queue = [] 
-    Tree.first.children.each { |child| queue << child }
-
-    while queue.size > 0
-      queue.each do |child|
-        if child._id == id 
-          goal = child
-        elsif child.has_children?
-          child.children.each { |child| queue << child }
-        end
-        queue.shift 
-      end
-    end
-    goal 
   end
 
   def has_children?
     !!self.children
   end
   
-
-
 end
