116bef40a606bc2dc9a6dcc9b6a88289b97f82a3
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-04-03 11:13:59 -0400

    the login and sign up buttons work from homepage, but they are not modals

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,11 @@
 //= require bootstrap
 //= require_welcome
 //= require d3
+
+$(document).on('submit', '#login_form', function(e) {
+    console.log("form was submitted!")
+}).on('ajax:success', '#login_form', function(e, data, status, xhr) {
+    console.log("success!")
+}).on('ajax:error', '#login_form', function(e, data, status, xhr) {
+    console.log("error!")
+});
