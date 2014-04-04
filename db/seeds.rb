81d7e588cec457889d9a0790310bbba45fdbe270
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-04-04 15:04:59 -0400

    trying to make seeds file work

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ original_data = %q(1. Call me Ishmael. -Herman Melville, Moby-Dick (1851)
 
 3. A screaming comes across the sky. -Thomas Pynchon, Gravity's Rainbow (1973)
 
-4. Many years later, as he faced the firing squad, Colonel Aureliano Buendia was to remember that distant afternoon when his father took him to discover ice. -Gabriel García Márquez, One Hundred Years of Solitude (1967; trans. Gregory Rabassa)
+4. Many years later, as he faced the firing squad, Colonel Aureliano Buendia was to remember that distant afternoon when his father took him to discover ice. -Gabriel Garcia Márquez, One Hundred Years of Solitude (1967; trans. Gregory Rabassa)
 
 5. Lolita, light of my life, fire of my loins. -Vladimir Nabokov, Lolita (1955)
 
@@ -209,60 +209,107 @@ original_data = %q(1. Call me Ishmael. -Herman Melville, Moby-Dick (1851)
 
 100. The cold passed reluctantly from the earth, and the retiring fogs revealed an army stretched out on the hills, resting. -Stephen Crane, The Red Badge of Courage (1895))
 
-original_data = original_data.split("\n\n")
+# original_data = original_data.split("\n\n")
 
-sentences = original_data.map do |quote|
-  /\. (?<sentence>.*)-/.match(quote)
-end
+# sentences = original_data.map do |quote|
+#   /\. (?<sentence>.*)-/.match(quote)
+# end
 
-sentences.compact!
+# sentences.compact!
 
-sentences = sentences.map do |quote|
-  quote[1].split("-")[0].strip
-end
+# sentences = sentences.map do |quote|
+#   quote[1].split("-")[0].strip
+# end
 
-sentences.reject! do |quote|
-  quote.length > 80
-end
+# sentences.reject! do |quote|
+#   quote.length > 80
+# end
 
-sentences.reject! do |quote|
-  /in a voice that rustled/.match(quote) || /shadow of the waxwing slain/.match(quote) || /You get justice in the next world/.match(quote) || /Drusus Nero Germanicus/.match(quote) || /stubborn and determined middle/.match(quote) || /died yesterday in his/.match(quote) || /of the New York Post/.match(quote)
-end
+# sentences.reject! do |quote|
+#   /in a voice that rustled/.match(quote) || /shadow of the waxwing slain/.match(quote) || /You get justice in the next world/.match(quote) || /Drusus Nero Germanicus/.match(quote) || /stubborn and determined middle/.match(quote) || /died yesterday in his/.match(quote) || /of the New York Post/.match(quote)
+# end
+
+sentences = %q(Call me Ishmael.
+A screaming comes across the sky.
+Lolita, light of my life, fire of my loins.
+Happy families are all alike; every unhappy family is unhappy in its own way.
+It was a bright cold day in April, and the clocks were striking thirteen.
+I am an invisible man.
+The Miss Lonelyhearts of the New York Post
+The sun shone, having no alternative, on the nothing new.
+This is the saddest story I have ever heard.
+Through the fence, between the curling flower spaces, I could see them hitting.
+124 was spiteful.
+Mother died today.
+Every summer Lin Kong returned to Goose Village to divorce his wife, Shuyu.
+The sky above the port was the color of television, tuned to a dead channel.
+I am a sick man . . . I am a spiteful man.
+Where now? Who now? When now?
+In a sense, I am Jacob Horner.
+It was like so, but wasn't.
+Mrs. Dalloway said she would buy the flowers herself.
+All this happened, more or less.
+They shoot the white girl first.
+For a long time, I went to bed early.
+The moment one learns English, complications set in.
+Dr. Weiss, at forty, knew that her life had been ruined by literature.
+Ships at a distance have every man's wish on board.
+There was a boy called Eustace Clarence Scrubb, and he almost deserved it.
+It was the day my grandmother exploded.
+Elmer Gantry was drunk.
+We started dying before the snow, and like the snow, we continued to fall.
+It was a pleasure to burn.
+In the beginning, sometimes I left messages in the street.
+It was love at first sight.
+I have never begun a novel with more misgiving.
+You better not never tell nobody but God.
+If I am out of my mind, it's all right with me, thought Moses Herzog.
+When Dick Gibson was a little boy he was not Dick Gibson.
+The past is a foreign country; they do things differently there.
+Vaughan died yesterday in his last car
+I write this sitting in the kitchen sink.
+He was born with a gift of laughter and a sense that the world was mad.
+Psychics can see the color of time it's blue.
+In the town, there were two mutes and they were always together.
+Time is not a line but a dimension, like the dimensions of space.
+They say when trouble comes close ranks, and so the white people did.)
+
+sentences = sentences.split("\n")
 
 sentences.each do |quote|
   Prompt.create(:content => quote)
 end
 
-user = User.last
-tree_one = Tree.new(:content => "Tumblr you probably haven't heard of them Intelligentsia. ", :title => "A visualization")
-tree_one.bind_user(user)
+# user = User.last
+# tree_one = Tree.new(:content => "Tumblr you probably haven't heard of them Intelligentsia. ", :title => "A visualization")
+# tree_one.bind_user(user)
 
-# text_old = "Umami retro put a bird on it, twee asymmetrical mustache Schlitz. \n+1 Etsy polaroid Marfa chia kale chips ennui asymmetrical bespoke Austin meh, pickled skateboard. Hoodie leggings organic selvage selfies, distillery hashtag mixtape irony umami you probably haven't heard of them normcore post-ironic. \n+1 dreamcatcher Odd Future leggings pork belly fap, biodiesel DIY distillery Truffaut. Readymade selvage Brooklyn, street art cray polaroid ennui cornhole actually Neutra. \nSynth pickled bitters seitan, umami aesthetic photo booth freegan Schlitz food truck. Tattooed wayfarers scenester, swag you probably haven't heard of them organic Cosby sweater salvia.Helvetica freegan raw denim PBR&B locavore. Chia artisan cray cardigan banjo. \n3 wolf moon brunch cardigan PBR&B Bushwick. Authentic 8-bit sriracha, Neutra distillery butcher squid. Lo-fi sriracha cray authentic, gentrify Bushwick aesthetic kogi chia mlkshk Cosby sweater VHS. Photo booth Williamsburg kale chips YOLO. Viral Cosby sweater retro, cliche letterpress paleo semiotics chambray fap biodiesel Intelligentsia wolf.McSweeney's art party Odd Future banh mi synth, American Apparel tote bag seitan lo-fi bitters fixie Neutra organic. Umami pop-up ethical pork belly pour-over Odd Future. Try-hard flannel twee, church-key seitan pop-up ethnic actually wayfarers biodiesel. Seitan Tonx Portland artisan. Tousled organic scenester pickled sustainable beard. Next level 90's Helvetica, hoodie craft beer artisan jean shorts leggings bitters chambray deep v Vice brunch Truffaut. Cardigan scenester actually ethical food truck, twee master cleanse cliche messenger bag craft beer mumblecore Neutra normcore biodiesel."
-text = "Umami retro put a bird on it, twee asymmetrical mustache Schlitz. +1 Etsy polaroid Marfa chia kale chips ennui asymmetrical bespoke Austin meh, pickled skateboard. Hoodie leggings organic selvage selfies, distillery hashtag mixtape irony umami you probably haven't heard of them normcore post-ironic. +1 dreamcatcher Odd Future leggings pork belly fap, biodiesel DIY distillery Truffaut. Readymade selvage Brooklyn, street art cray polaroid ennui cornhole actually Neutra. Synth pickled bitters seitan, umami aesthetic photo booth freegan Schlitz food truck. Tattooed wayfarers scenester, swag you probably haven't heard of them organic Cosby sweater salvia.
+# # text_old = "Umami retro put a bird on it, twee asymmetrical mustache Schlitz. \n+1 Etsy polaroid Marfa chia kale chips ennui asymmetrical bespoke Austin meh, pickled skateboard. Hoodie leggings organic selvage selfies, distillery hashtag mixtape irony umami you probably haven't heard of them normcore post-ironic. \n+1 dreamcatcher Odd Future leggings pork belly fap, biodiesel DIY distillery Truffaut. Readymade selvage Brooklyn, street art cray polaroid ennui cornhole actually Neutra. \nSynth pickled bitters seitan, umami aesthetic photo booth freegan Schlitz food truck. Tattooed wayfarers scenester, swag you probably haven't heard of them organic Cosby sweater salvia.Helvetica freegan raw denim PBR&B locavore. Chia artisan cray cardigan banjo. \n3 wolf moon brunch cardigan PBR&B Bushwick. Authentic 8-bit sriracha, Neutra distillery butcher squid. Lo-fi sriracha cray authentic, gentrify Bushwick aesthetic kogi chia mlkshk Cosby sweater VHS. Photo booth Williamsburg kale chips YOLO. Viral Cosby sweater retro, cliche letterpress paleo semiotics chambray fap biodiesel Intelligentsia wolf.McSweeney's art party Odd Future banh mi synth, American Apparel tote bag seitan lo-fi bitters fixie Neutra organic. Umami pop-up ethical pork belly pour-over Odd Future. Try-hard flannel twee, church-key seitan pop-up ethnic actually wayfarers biodiesel. Seitan Tonx Portland artisan. Tousled organic scenester pickled sustainable beard. Next level 90's Helvetica, hoodie craft beer artisan jean shorts leggings bitters chambray deep v Vice brunch Truffaut. Cardigan scenester actually ethical food truck, twee master cleanse cliche messenger bag craft beer mumblecore Neutra normcore biodiesel."
+# text = "Umami retro put a bird on it, twee asymmetrical mustache Schlitz. +1 Etsy polaroid Marfa chia kale chips ennui asymmetrical bespoke Austin meh, pickled skateboard. Hoodie leggings organic selvage selfies, distillery hashtag mixtape irony umami you probably haven't heard of them normcore post-ironic. +1 dreamcatcher Odd Future leggings pork belly fap, biodiesel DIY distillery Truffaut. Readymade selvage Brooklyn, street art cray polaroid ennui cornhole actually Neutra. Synth pickled bitters seitan, umami aesthetic photo booth freegan Schlitz food truck. Tattooed wayfarers scenester, swag you probably haven't heard of them organic Cosby sweater salvia.
 
-Helvetica freegan raw denim PBR&B locavore. Chia artisan cray cardigan banjo. 3 wolf moon brunch cardigan PBR&B Bushwick. Authentic 8-bit sriracha, Neutra distillery butcher squid. Lo-fi sriracha cray authentic, gentrify Bushwick aesthetic kogi chia mlkshk Cosby sweater VHS. Photo booth Williamsburg kale chips YOLO. Viral Cosby sweater retro, cliche letterpress paleo semiotics chambray fap biodiesel Intelligentsia wolf.
+# Helvetica freegan raw denim PBR&B locavore. Chia artisan cray cardigan banjo. 3 wolf moon brunch cardigan PBR&B Bushwick. Authentic 8-bit sriracha, Neutra distillery butcher squid. Lo-fi sriracha cray authentic, gentrify Bushwick aesthetic kogi chia mlkshk Cosby sweater VHS. Photo booth Williamsburg kale chips YOLO. Viral Cosby sweater retro, cliche letterpress paleo semiotics chambray fap biodiesel Intelligentsia wolf.
 
-McSweeney's art party Odd Future banh mi synth, American Apparel tote bag seitan lo-fi bitters fixie Neutra organic. Umami pop-up ethical pork belly pour-over Odd Future. Try-hard flannel twee, church-key seitan pop-up ethnic actually wayfarers biodiesel. Seitan Tonx Portland artisan. Tousled organic scenester pickled sustainable beard. Next level 90's Helvetica, hoodie craft beer artisan jean shorts leggings bitters chambray deep v Vice brunch Truffaut. Cardigan scenester actually ethical food truck, twee master cleanse cliche messenger bag craft beer mumblecore Neutra normcore biodiesel."
-# experiment= "Umami\n. retro\n. put\n. a\n. bird\n. on\n. it"
+# McSweeney's art party Odd Future banh mi synth, American Apparel tote bag seitan lo-fi bitters fixie Neutra organic. Umami pop-up ethical pork belly pour-over Odd Future. Try-hard flannel twee, church-key seitan pop-up ethnic actually wayfarers biodiesel. Seitan Tonx Portland artisan. Tousled organic scenester pickled sustainable beard. Next level 90's Helvetica, hoodie craft beer artisan jean shorts leggings bitters chambray deep v Vice brunch Truffaut. Cardigan scenester actually ethical food truck, twee master cleanse cliche messenger bag craft beer mumblecore Neutra normcore biodiesel."
+# # experiment= "Umami\n. retro\n. put\n. a\n. bird\n. on\n. it"
 
-text = text.gsub(". ", ".. ").split(". ")
+# text = text.gsub(". ", ".. ").split(". ")
 
-def add_children_again(number, tree, sample_text)
-  tree_child_one = tree.child_trees.build(:content => " "+sample_text.sample+" "+sample_text.sample)
-  tree_child_two = tree.child_trees.build(:content => " "+sample_text.sample+" "+sample_text.sample)
-  tree_child_three = tree.child_trees.build(:content => " "+sample_text.sample+" "+sample_text.sample)
-  tree.get_root.contributor_count += 3
-  tree.get_root.save
-  arr = [tree_child_one, tree_child_two, tree_child_three]
-  tree_child_one.save
-  tree_child_two.save
-  tree_child_three.save
-  if number != 0
-    add_children_again(number-1, arr.sample, sample_text)
-    add_children_again(number-1, arr.sample, sample_text)
-    add_children_again(number-1, arr.sample, sample_text)
-  end  
-end
+# def add_children_again(number, tree, sample_text)
+#   tree_child_one = tree.child_trees.build(:content => " "+sample_text.sample+" "+sample_text.sample)
+#   tree_child_two = tree.child_trees.build(:content => " "+sample_text.sample+" "+sample_text.sample)
+#   tree_child_three = tree.child_trees.build(:content => " "+sample_text.sample+" "+sample_text.sample)
+#   tree.get_root.contributor_count += 3
+#   tree.get_root.save
+#   arr = [tree_child_one, tree_child_two, tree_child_three]
+#   tree_child_one.save
+#   tree_child_two.save
+#   tree_child_three.save
+#   if number != 0
+#     add_children_again(number-1, arr.sample, sample_text)
+#     add_children_again(number-1, arr.sample, sample_text)
+#     add_children_again(number-1, arr.sample, sample_text)
+#   end  
+# end
 
-add_children_again(2, tree_one, text)
-tree_one.save
+# add_children_again(2, tree_one, text)
+# tree_one.save
