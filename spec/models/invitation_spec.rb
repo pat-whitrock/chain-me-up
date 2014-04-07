140a8f69cf87a450ff7dfaae5443a3c0eb36cedb
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-07 12:08:16 -0400

    invitation create token creates a token

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -4,13 +4,15 @@ describe Invitation do
 
 	let(:tree) {Tree.create(:title => "Test", :content => "Test content")}
 
-  let(:invitation) {Invitation.create(:email => "testing@test.com", :tree => :tree)}
+  let(:invitation) {Invitation.create(:email => "testing@test.com")}
 
   let(:user) {create(:user)}
 
   describe "#create_token" do 
     it "creates a token for the invitation" do
-      
+      @invitation = invitation
+
+      @invitation.token.should_not be_nil
     end
   end
 
